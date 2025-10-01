fcbd06fe44fc2118a8b1c06a37bb0d42cb99eee4
Gianluca Sciabarrasi <gianluca.sciabarrasi@crombie.dev>
2025-10-01 10:07:59 -0300

    feat: add Python fundamentals exercises and examples

---
+++ fundamentos/datos_escalares.py
@@ -0,0 +1,5 @@
+x = 5 # INT type
+y = 3.14 # FLOAT type
+name = "Gianluca" # STRING type
+is_student = True # BOOLEAN type
+print(x + y)
---
+++ fundamentos/operadores.py
@@ -0,0 +1,7 @@
+#Operadores aritmeticos y de comparacion
+3+4 # = 7
+5-9 # = -4
+2*3 # = 6
+8/4 # = 2.0
+7//3 # = 2 (division entera)
+7%3 # = 1 (modulo o resto de la division)
